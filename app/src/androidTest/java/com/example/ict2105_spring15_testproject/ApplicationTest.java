f3d3264c115564855dc46a34e7af82fc7a2f8b9c
doubleristretto <doubleristretto@users.noreply.github.com>
2015-01-14 14:11:47 +0800

    Add example code files

---
+++ app/src/androidTest/java/com/example/ict2105_spring15_testproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.ict2105_spring15_testproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
