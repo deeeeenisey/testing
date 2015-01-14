e7ecca6733f38061fc4f4ebf9852bd476c11267b
doubleristretto <doubleristretto@users.noreply.github.com>
2015-01-14 14:21:02 +0800

    Add logging message

--- app/src/main/java/com/example/ict2105_spring15_testproject/MainActivity.java
+++ app/src/main/java/com/example/ict2105_spring15_testproject/MainActivity.java
@@ -2,16 +2,19 @@ package com.example.ict2105_spring15_testproject;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TEST", "onCreate() Called");
     }
 
 
